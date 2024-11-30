0e6f244e24de5ef7a75249cc68e2de9bcb67be7f
asher <alexetik@yandex.ru>
2024-11-30 14:30:48 +0000

    Revert "правки"
    This reverts commit 7b8301223224add9d8857ca83143df443eead428.

--- llm_tools/tokens.py
+++ llm_tools/tokens.py
@@ -1,7 +1,6 @@
-import logging
 from dataclasses import dataclass
 from typing import List, Dict
-
+import logging
 import tiktoken
 
 from llm_tools.chat_message import (
@@ -34,16 +33,20 @@ class TokenExpense:
 
     def price_per_1e6_input_tokens(self) -> int:
         return {
-            "gpt-3.5-turbo": 2,
+            "gpt-3.5-turbo": 1,
             "gpt-4": 30,
-            "gpt-4-1106-preview": 10,
+            "gpt-4-turbo": 10,
+            "gpt-4o": 5,
+            "gpt-4o-mini": 1,  # TODO не корректно, надо переводить на double (0.3)
         }[self.model_name]
 
     def price_per_1e6_output_tokens(self) -> int:
         return {
-            "gpt-3.5-turbo": 2,
+            "gpt-3.5-turbo": 1,
             "gpt-4": 60,
-            "gpt-4-1106-preview": 30,
+            "gpt-4-turbo": 30,
+            "gpt-4o": 15,
+            "gpt-4o-mini": 1,  # TODO не корректно, надо переводить на double (1.2)
         }[self.model_name]
 
     def get_price_multiplied_by_1e6(self) -> int:
@@ -108,7 +111,7 @@ def count_tokens_from_output_text(
     return len(encoding.encode(text))
 
 
-def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613"):
+def num_tokens_from_messages(messages, model="gpt-3.5-turbo"):
     """Copied from https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
     """
     try:
@@ -117,13 +120,11 @@ def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613"):
         logger.debug("Warning: model not found. Using cl100k_base encoding.")
         encoding = tiktoken.get_encoding("cl100k_base")
     if model in {
-        "gpt-3.5-turbo-0613",
-        "gpt-3.5-turbo-16k-0613",
-        "gpt-4-0314",
-        "gpt-4-32k-0314",
-        "gpt-4-0613",
-        "gpt-4-32k-0613",
-        "gpt-4-1106-preview",
+        "gpt-3.5-turbo",
+        "gpt-4",
+        "gpt-4-turbo",
+        "gpt-4o",
+        "gpt-4o-mini",
     }:
         tokens_per_message = 3
         tokens_per_name = 1
@@ -131,11 +132,17 @@ def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613"):
         tokens_per_message = 4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
         tokens_per_name = -1  # if there's a name, the role is omitted
     elif "gpt-3.5-turbo" in model:
-        logger.debug("Warning: gpt-3.5-turbo may update over time. Returning num tokens assuming gpt-3.5-turbo-0613.")
-        return num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613")
+        logger.debug("Warning: gpt-3.5-turbo may update over time. Returning num tokens assuming gpt-3.5-turbo.")
+        return num_tokens_from_messages(messages, model="gpt-3.5-turbo")
     elif "gpt-4" in model:
-        logger.debug("Warning: gpt-4 may update over time. Returning num tokens assuming gpt-4-0613.")
-        return num_tokens_from_messages(messages, model="gpt-4-0613")
+        logger.debug("Warning: gpt-4 may update over time. Returning num tokens assuming pt-4.")
+        return num_tokens_from_messages(messages, model="gpt-4")
+    elif "gpt-4o" in model:
+        logger.debug("Warning: gpt-4 may update over time. Returning num tokens assuming gpt-4o.")
+        return num_tokens_from_messages(messages, model="gpt-4o")
+    elif "gpt-4o-mini" in model:
+        logger.debug("Warning: gpt-4 may update over time. Returning num tokens assuming gpt-4o-mini.")
+        return num_tokens_from_messages(messages, model="gpt-4o-mini")
     else:
         raise NotImplementedError(
             f"""num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens."""
@@ -144,8 +151,24 @@ def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613"):
     for message in messages:
         num_tokens += tokens_per_message
         for key, value in message.items():
-            num_tokens += len(encoding.encode(value))
+            # Check if the value is a list
+            if isinstance(value, list):
+                for item in value:
+                    # Check for the 'image_url' type within each dictionary in the list
+                    if isinstance(item, dict) and item.get("type") == "image_url":
+                        # num_tokens += 85 #openai берет такую цену при low качестве за каждую картинку
+                        #пропускаем, так как подсчитывается в приложении
+                        pass
+                    else:
+                        # Encode the value if it's not an image
+                        num_tokens += len(encoding.encode(str(item)))
+            else:
+                # Handle non-list values
+                num_tokens += len(encoding.encode(value))
+
             if key == "name":
                 num_tokens += tokens_per_name
+
     num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
     return num_tokens
+
